eaedadcd77775a6bbec6a127c82d2a3c7b59eff6
leewr <121657771@qq.com>
2018-12-13 09:14:18 +0800

    lua

--- ImageResizer.lua
+++ ImageResizer.lua
@@ -1,8 +1,19 @@
 
-local command = "convert  " .. ngx.var.request_filepath .. " -resize " .. ngx.var.width .. "x" .. ngx.var.height .. " +profile \"*\" " .. ngx.var.request_filepath .. "_" .. ngx.var.width .. "x" .. ngx.var.height .. "." .. ngx.var.ext
+local sizeType
 
-if ngx.var.webp == ".webp" then
-    command = command..ngx.var.webp
+if ngx.var.enforce == '!'
+then
+    sizeType = "!"
+else
+    sizeType = ""
 end
+
+local command = "convert  " ..ngx.var.request_filepath.." -resize ".. ngx.var.width .. "x" .. ngx.var.height .. ngx.var.enforce .. " +profile \"*\" " .. ngx.var.request_filepath .. "_" .. ngx.var.width .. "x" .. ngx.var.height .. ngx.var.enforce .. "." .. ngx.var.ext
+if ngx.var.webp == ".webp" 
+then
+    command = command ..ngx.var.webp
+end
+-- ngx.say(command)
 os.execute(command)
 ngx.exec(ngx.var.request_uri)
+
