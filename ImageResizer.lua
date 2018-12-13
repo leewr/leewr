ff23c733dfb1b92684de67782251fefaf3d949ca
liwenrun <liwenrunWj65423@touna.cn>
2018-12-13 17:00:43 +0800

    添加nginx图片剪裁功能、修改lua convert命令

--- ImageResizer.lua
+++ ImageResizer.lua
@@ -1,14 +1,16 @@
 
 local sizeType
-
-if ngx.var.enforce == '!'
+local command
+if ngx.var.enforce == 'f'
 then
-    sizeType = "!"
+    command = "convert "..ngx.var.request_filepath.." -resize "..ngx.var.width.."x"..ngx.var.height.."^ -gravity center -extent ".. ngx.var.width .. "x" .. ngx.var.height .." "..ngx.var.request_filepath.."_"..ngx.var.width.."x"..ngx.var.height..ngx.var.enforce.."."..ngx.var.ext
+    sizeType = "f"
 else
+    command = "convert " ..ngx.var.request_filepath.." -resize ".. ngx.var.width .. "x" .. ngx.var.height .. " +profile \"*\" " .. ngx.var.request_filepath .. "_" .. ngx.var.width .. "x" .. ngx.var.height .. ngx.var.enforce .. "." .. ngx.var.ext
     sizeType = ""
 end
 
-local command = "convert  " ..ngx.var.request_filepath.." -resize ".. ngx.var.width .. "x" .. ngx.var.height .. ngx.var.enforce .. " +profile \"*\" " .. ngx.var.request_filepath .. "_" .. ngx.var.width .. "x" .. ngx.var.height .. ngx.var.enforce .. "." .. ngx.var.ext
+-- local command = "convert  " ..ngx.var.request_filepath.." -resize ".. ngx.var.width .. "x" .. ngx.var.height .. ngx.var.enforce .. " +profile \"*\" " .. ngx.var.request_filepath .. "_" .. ngx.var.width .. "x" .. ngx.var.height .. ngx.var.enforce .. "." .. ngx.var.ext
 if ngx.var.webp == ".webp" 
 then
     command = command ..ngx.var.webp
