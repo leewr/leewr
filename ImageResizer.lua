e1fa412492457511a39b2b8ae21545fbfc2e90d4
leewr <121657771@qq.com>
2018-12-16 17:38:17 +0800

    修改nginx、lua、webp.lua

--- ImageResizer.lua
+++ ImageResizer.lua
@@ -5,20 +5,17 @@ if ngx.var.enforce == 'f'
 then
     command = "convert "..ngx.var.request_filepath.." -resize "..ngx.var.width.."x"..ngx.var.height.."^ -gravity center -extent ".. ngx.var.width .. "x" .. ngx.var.height .." "..ngx.var.request_filepath.."_"..ngx.var.width.."x"..ngx.var.height..ngx.var.enforce.."."..ngx.var.ext
     sizeType = "f"
-elseif ngx.var.noresize == {} and ngx.var.webp == ".webp"
-then
-    command = "convert " ..ngx.var.request_filepath.." "..ngx.var.request_filepath .. ngx.var.webp .."ssss"
 else
     command = "convert " ..ngx.var.request_filepath.." -resize ".. ngx.var.width .. "x" .. ngx.var.height .. " +profile \"*\" " .. ngx.var.request_filepath .. "_" .. ngx.var.width .. "x" .. ngx.var.height .. ngx.var.enforce .. "." .. ngx.var.ext
     sizeType = ""
 end
 
 -- local command = "convert  " ..ngx.var.request_filepath.." -resize ".. ngx.var.width .. "x" .. ngx.var.height .. ngx.var.enforce .. " +profile \"*\" " .. ngx.var.request_filepath .. "_" .. ngx.var.width .. "x" .. ngx.var.height .. ngx.var.enforce .. "." .. ngx.var.ext
-if ngx.var.webp == ".webp" and ngx.var.noresize ~= ""
+if ngx.var.webp == ".webp"
 then
     command = command ..ngx.var.webp
 end
-ngx.say(ngx.var.noresize)
---os.execute(command)
---ngx.exec(ngx.var.request_uri)
+-- ngx.say(ngx.var.noresize)
+os.execute(command)
+ngx.exec(ngx.var.request_uri)
 
