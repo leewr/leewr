2851b5d7615f774b64a7a4dd4e83c9bb633c25dd
leewr <121657771@qq.com>
2018-12-16 17:37:30 +0800

    december

--- app/public/december/gulpfile.js
+++ app/public/december/gulpfile.js
@@ -2,7 +2,11 @@
  
 var gulp = require('gulp');
 var less = require('gulp-less');
-var path = require('path')
+var sass = require('gulp-sass');
+var path = require('path');
+sass.compiler = require('node-sass');
+var convert = require('webp-batch-convert')
+
 
 gulp.task('default', function() {
 	return gulp.src('./scss/*.less')
@@ -12,4 +16,21 @@ gulp.task('default', function() {
 	    .pipe(gulp.dest('./css'));
 });
 
+
+
+
+gulp.task('sass', function () {
+  return gulp.src('./scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+
+gulp.task('webp', function () {
+	var res = convert.cwebp('./images', './image', {
+		q: 80,
+		quiet: true
+	})
+	console.log('total is: ' + res)
+})
  
