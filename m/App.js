d41a879f3a1b11fa3bca4551b011e2cf7b7815da
liwenrun <liwenrunWj65423@touna.cn>
2019-01-08 19:34:19 +0800

    vscode setting

--- m/App.js
+++ m/App.js
@@ -1,7 +1,5 @@
-import React from "react";
+import React from "react"
 
-const About = () => (
-    <h2>This is the about page</h2>
-);
+const About = () => <h2>This is the about page</h2>
 
-export default About;
+export default About
